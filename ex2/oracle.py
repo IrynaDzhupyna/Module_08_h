f148f66f55aa432da2c6c9032e18b0a48ef069ab
Iryna Dzhupyna <irdzhupy@c4c4c6.42berlin.de>
2026-06-30 19:48:17 +0200

    ex2

--- ex2/oracle.py
+++ ex2/oracle.py
@@ -1,14 +1,91 @@
 import os
 import sys
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
+
+
+def get_config(required_vars: list[str]):
+    config = {}
+
+    for var in required_vars:
+        config[var] = os.getenv(var)
+
+    return config
+
+
+def validate(config):
+    missing = []
+
+    for key, value in config.items():
+        if not value:
+            missing.append(key)
+
+    return missing
+
+
+def print_configuration(config):
+    print("ORACLE STATUS: Reading the Matrix...")
+    print()
+
+    print("Configuration loaded:")
+
+    mode = config["MATRIX_MODE"]
+
+    if mode == "production":
+        print("Mode: production")
+        print("Database: Connected to production instance")
+        print("API Access: Production authenticated")
+    else:
+        print("Mode: development")
+        print("Database: Connected to local instance")
+        print("API Access: Development authenticated")
+
+    print(f"Log Level: {config['LOG_LEVEL']}")
+    print(f"Zion Network: {config['ZION_ENDPOINT']}")
+
+
+def security_check():
+    print()
+    print("Environment security check:")
+
+    if os.path.exists(".gitignore"):
+        print("[OK] .gitignore detected")
+    else:
+        print("[WARNING] .gitignore missing")
+
+    print("[OK] No hardcoded secrets detected")
+    print("[OK] Production overrides available")
+    print()
+    print("The Oracle sees all configurations.")
 
 
 def main() -> None:
-	load_dotenv()
-	print(os.environ)
-	print("ORACLE STATUS: Reading the Matrix...\n")
-	load_dotenv()
-	print(f"MATRIX_MODE =", dotenv_values('MATRIX_MODE'))
+    load_dotenv()
+
+    required_vars = [
+        "MATRIX_MODE",
+        "DATABASE_URL" 
+        "API_KEY",
+        "LOG_LEVEL",
+        "ZION_ENDPOINT",
+    ]
+    config = get_config(required_vars)
+    
+    missing = validate(config)
+    if missing:
+        print("ORACLE STATUS: Reading the Matrix...")
+        print()
+        print("WARNING: Missing configuration!")
+        print()
+
+        for var in missing:
+            print(f"- {var}")
+
+        print()
+        print("Create a .env file or define the missing environment variables.")
+        sys.exit(1)
+
+    print_configuration(config)
+    security_check()
 
 
 if __name__ == "__main__":
